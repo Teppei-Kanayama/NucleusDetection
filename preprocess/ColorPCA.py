76043151c5b9bce85dd3f13260bba6af5aa0b981
sukanpin <mashu.tsujita@gmail.com>
2018-03-10 18:35:56 +0900

    fix

--- preprocess/ColorPCA.py
+++ preprocess/ColorPCA.py
@@ -1,20 +1,18 @@
-
-# coding: utf-8
-
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 class ColorPCA:#なお結果画像に変化はなかった模様。
     data = 0
     def __init__(self,org_data):
-        org_re = org_data.reshape(-1,4)
+        z_num = org_data.shape[2]
+        org_re = org_data.reshape(-1,z_num)
         org_mean=np.mean(org_data,axis=(0,1))
         mean, eigenvectors = cv2.PCACompute(
             data=org_re,
             mean=org_mean.reshape(1,-1),
             maxComponents=1
         )
-        vec = eigenvectors.reshape(4)
+        vec = eigenvectors.reshape(z_num)
         eigen_data =org_data.dot(vec)
         global data
         data = (eigen_data-np.nanmin(eigen_data))/(np.nanmax(eigen_data)-np.nanmin(eigen_data))*255
