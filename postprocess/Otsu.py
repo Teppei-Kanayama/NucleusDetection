6da34f66d8f30ecb5acc4341781486ad5ea18274
sukanpin <mashu.tsujita@gmail.com>
2018-03-10 18:18:32 +0900

    くびれ検出・imread消去

--- postprocess/Otsu.py
+++ postprocess/Otsu.py
@@ -4,9 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 class OTSU:
     __data = 0
-    def __init__(self,file_name):
-        src = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
-        ret, self.__data = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)
+    def __init__(self,data):#dataはグレースケール画像である必要あり
+        ret, self.__data = cv2.threshold(data, 0, 255, cv2.THRESH_OTSU)
     def show(self):
         plt.imshow(self.__data)
         plt.gray()
--- preprocess/ColorPCA.py
+++ preprocess/ColorPCA.py
@@ -6,8 +6,7 @@ import cv2
 import matplotlib.pyplot as plt
 class ColorPCA:#なお結果画像に変化はなかった模様。
     data = 0
-    def __init__(self,file_name):
-        org_data = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
+    def __init__(self,org_data):
         org_re = org_data.reshape(-1,4)
         org_mean=np.mean(org_data,axis=(0,1))
         mean, eigenvectors = cv2.PCACompute(
