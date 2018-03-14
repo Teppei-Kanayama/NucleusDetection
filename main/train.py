fc30d190397cf5b708cb14369d995df631ed0e26
TeppeiKanayama <1994teppei0615kanayama@gmail.com>
2018-03-14 14:48:50 +0900

    paser周りを整理した

--- main/train.py
+++ main/train.py
@@ -7,8 +7,7 @@ import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
 import torch.nn as nn
 
-from utils import load
-from utils import utils
+from utils import load, utils, option_manager
 from unet import UNet
 from torch.autograd import Variable
 from torch import optim
@@ -24,49 +23,50 @@ from myloss import weighted_binary_cross_entropy
 from validation import validate
 
 SIZE = (640, 640)
-EXP_ID = 7
 
-def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=1, lr=0.1, val_percent=0.05, cp=True, gpu=False, calc_score=True):
+def train_net(options):
+    data = options.data
     dir_img = data + '/images/'
     dir_mask = data + '/masks/'
     dir_edge = data + '/edges/'
-    dir_save = save
+    dir_save = options.save
     ids = load.get_ids(dir_img)
 
     # trainとvalに分ける
-    iddataset = utils.split_train_val(ids, val_percent)
-
-    print('''
-    Starting training:
-        Epochs: {}
-        Batch size: {}
-        Learning rate: {}
-        Training size: {}
-        Validation size: {}
-        Checkpoints: {}
-        CUDA: {}
-    '''.format(epochs, batch_size, lr, len(iddataset['train']),
-               len(iddataset['val']), str(cp), str(gpu)))
-
+    iddataset = utils.split_train_val(ids, options.val_percent)
     N_train = len(iddataset['train'])
     N_val = len(iddataset['val'])
-    N_batch_per_epoch_train = int(N_train / batch_size)
-    N_batch_per_epoch_val = int(N_val / val_batch_size)
+    N_batch_per_epoch_train = int(N_train / options.batchsize)
+    N_batch_per_epoch_val = int(N_val / options.val_batchsize)
+
+    option_manager.display_info(options, N_train, N_val)
+
+
+    # モデルの定義
+    net = UNet(3, 1)
+
+    # 学習済みモデルをロードする
+    if options.load:
+        net.load_state_dict(torch.load(options.load))
+        print('Model loaded from {}'.format(options.load))
+
+    if options.gpu:
+        net.cuda()
+
     # 最適化手法を定義
     #optimizer = optim.SGD(net.parameters(),
     #                      lr=lr, momentum=0.9, weight_decay=0.0005)
     optimizer = optim.Adam(net.parameters())
-    criterion = nn.BCELoss()
 
     # 学習開始
     train_loss_list = []
     validation_loss_list = []
-    validation_score_matrix = np.zeros((epochs, 10))
+    validation_score_matrix = np.zeros((options.epochs, 10))
     validation_score_list = []
 
 
-    for epoch in range(epochs):
-        print('Starting epoch {}/{}.'.format(epoch+1, epochs))
+    for epoch in range(options.epochs):
+        print('Starting epoch {}/{}.'.format(epoch+1, options.epochs))
         train = load.get_imgs_and_masks(iddataset['train'], dir_img, dir_mask, dir_edge, SIZE)
         original_sizes = load.get_original_sizes(iddataset['val'], dir_img, '.png')
         val = load.get_imgs_and_masks(iddataset['val'], dir_img, dir_mask, dir_edge, SIZE, train=False)
@@ -77,7 +77,7 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
 
         # training phase
         net.train()
-        for i, b in enumerate(utils.batch(train, batch_size)):
+        for i, b in enumerate(utils.batch(train, options.batchsize)):
             X = np.array([j[0] for j in b])
             y = np.array([j[1] for j in b])
             w = np.array([j[2] for j in b])
@@ -88,7 +88,7 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             y = torch.ByteTensor(y)
             w = torch.ByteTensor(w)
 
-            if gpu:
+            if options.gpu:
                 X = X.cuda()
                 y = y.cuda()
                 w = w.cuda()
@@ -104,10 +104,9 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             w_flat = w.view(-1)
             weight = (w_flat.float() / 255.) * 4. + 1.
             loss = weighted_binary_cross_entropy(probs_flat, y_flat.float() / 255., weight)
-            #loss = criterion(probs_flat, y_flat.float() / 255.)
             train_loss += loss.data[0]
 
-            print('{0:.4f} --- loss: {1:.6f}'.format(i*batch_size/N_train,
+            print('{0:.4f} --- loss: {1:.6f}'.format(i*options.batchsize/N_train,
                                                      loss.data[0]))
 
             optimizer.zero_grad()
@@ -119,7 +118,7 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
 
         # validation phase
         net.eval()
-        for i, b in enumerate(utils.batch(val, val_batch_size)):
+        for i, b in enumerate(utils.batch(val, options.val_batchsize)):
             X = np.array([j[0] for j in b])[:, :3, :, :] # alpha channelを取り除く
             y = np.array([j[1] for j in b])
             w = np.array([j[2] for j in b])
@@ -127,7 +126,7 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             y = torch.ByteTensor(y)
             w = torch.ByteTensor(w)
 
-            if gpu:
+            if options.gpu:
                 X = X.cuda()
                 y = y.cuda()
                 w = w.cuda()
@@ -145,7 +144,6 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             # edgeに対して重み付けをする
             weight = (w_flat.float() / 255.) * 4. + 1.
             loss = weighted_binary_cross_entropy(probs_flat, y_flat.float() / 255., weight)
-            #loss = criterion(probs_flat, y_flat.float() / 255.)
             validation_loss += loss.data[0]
 
             y_hat = np.asarray((probs > 0.5).data)
@@ -153,7 +151,7 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             y_truth = np.asarray(y.data)
 
             # calculate validatation score
-            if calc_score and False:  # 初期はノイズが多くスコアリングに時間がかかるため
+            if options.calc_score and False:  # 初期はノイズが多くスコアリングに時間がかかるため
                 print("Image No.", i, "started.")
                 score, scores, _ = validate(y_hat, y_truth)
                 validation_score += score
@@ -162,21 +160,21 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             # save images
             result = Image.fromarray((y_hat * 255).astype(np.uint8))
             result = result.resize(original_sizes[i])
-            result.save(save_val + iddataset['val'][i] + ".png")
+            result.save(options.save_val + iddataset['val'][i] + ".png")
 
         print('Val Loss: {}'.format(validation_loss / N_batch_per_epoch_val))
         validation_loss_list.append(validation_loss / N_batch_per_epoch_val)
 
-        if calc_score:
+        if options.calc_score:
             print('score: {}'.format(validation_score / i))
             validation_score_matrix[epoch] = validation_scores / i
             validation_score_list.append(validation_score / i)
         else:
             validation_score_list.append(0.0)
 
-        if cp and (epoch + 1) % 10 == 0:
+        if (epoch + 1) % 10 == 0:
             torch.save(net.state_dict(),
-                       dir_save + str(EXP_ID) + '_CP{}.pth'.format(epoch+1))
+                       dir_save + str(options.id) + '_CP{}.pth'.format(epoch+1))
 
             print('Checkpoint {} saved !'.format(epoch+1))
 
@@ -188,11 +186,11 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
         plt.title("Mean WBCELoss")
         plt.xlabel("epochs")
         plt.ylabel("loss")
-        plt.savefig("./results/loss" + str(EXP_ID) + ".png")
+        plt.savefig("./results/loss" + str(options.id) + ".png")
         plt.clf()
 
         # draw score graph
-        if calc_score:
+        if options.calc_score:
             plt.plot(epoch_arange, validation_score_list, label="Mean", linewidth=4)
             threshold = np.linspace(0.5, 0.95, 10)
             threshold = np.around(threshold, decimals=2)
@@ -206,45 +204,9 @@ def train_net(net, data, save, save_val, epochs=5, batch_size=2, val_batch_size=
             plt.savefig("./results/score.png")
             plt.clf()
 
+
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-e', '--epochs', dest='epochs', default=5, type='int',
-                      help='number of epochs')
-    parser.add_option('-b', '--batchsize', dest='batchsize', default=10,
-                      type='int', help='batch size')
-    parser.add_option('-v', '--val_batch_size', dest='val_batch_size', default=1,
-                      type='int', help='validation batch size')
-    parser.add_option('-l', '--learning-rate', dest='lr', default=0.1,
-                      type='float', help='learning rate')
-    parser.add_option('-g', '--gpu', action='store_true', dest='gpu',
-                      default=False, help='use cuda')
-    parser.add_option('-c', '--load', dest='load',
-                      default=False, help='load file model')
-    parser.add_option('-d', '--data', dest='data',
-                      default='/data/unagi0/kanayama/dataset/nuclei_images/stage1_train_default', help='path to training data')
-    parser.add_option('-s', '--save', dest='save',
-                      default='/data/unagi0/kanayama/dataset/nuclei_images/checkpoints/',
-                      help='path to save models')
-    parser.add_option('--save_val', dest='save_val',
-                      default='/data/unagi0/kanayama/dataset/nuclei_images/answer_val/',
-                      help='path to save models')
-    parser.add_option('--calc_score', dest='calc_score',
-                      default=True,
-                      help='whether calculate score or not')
-
-    (options, args) = parser.parse_args()
-
-    # モデルの定義
-    net = UNet(3, 1)
-
-    # 学習済みモデルをロードする
-    if options.load:
-        net.load_state_dict(torch.load(options.load))
-        print('Model loaded from {}'.format(options.load))
-
-    if options.gpu:
-        net.cuda()
+    (options, args) = option_manager.parse()
 
     #学習を実行
-    train_net(net, options.data, options.save, options.save_val, options.epochs, options.batchsize, options.val_batch_size, options.lr,
-              gpu=options.gpu, calc_score=options.calc_score)
+    train_net(options)
---
+++ main/utils/option_manager.py
@@ -0,0 +1,47 @@
+import os
+from optparse import OptionParser
+
+def parse():
+    parser = OptionParser()
+    parser.add_option('--id', type='int',
+                      help='ID of Experiment')
+    parser.add_option('--epochs', default=5, type='int',
+                      help='number of epochs')
+    parser.add_option('--batchsize', default=10,
+                      type='int', help='batch size')
+    parser.add_option('--val_batchsize', default=1,
+                      type='int', help='validation batch size')
+    parser.add_option('--val_percent', default=0.05,
+                      type='float', help='validation batch size')
+    parser.add_option('--lr', default=0.1,
+                      type='float', help='learning rate')
+    parser.add_option('--gpu', action='store_true',
+                      default=False, help='use cuda')
+    parser.add_option('--load',
+                      default=False, help='load file model')
+    parser.add_option('--data',
+                      default='/data/unagi0/kanayama/dataset/nuclei_images/stage1_train_default', help='path to training data')
+    parser.add_option('--save',
+                      default='/data/unagi0/kanayama/dataset/nuclei_images/checkpoints/',
+                      help='path to save models')
+    parser.add_option('--save_val',
+                      default='/data/unagi0/kanayama/dataset/nuclei_images/answer_val/',
+                      help='path to save models')
+    parser.add_option('--calc_score', action='store_true'
+                      default=False,
+                      help='whether calculate score or not')
+
+    return parser.parse_args()
+
+
+def display_info(options, N_train, N_val):
+    print('+------------------------------+')
+    print('| U-Net Segmentation')
+    print('+------------------------------+')
+    print('| Epochs  : {}'.format(options.epochs))
+    print('| Batch size  : {}'.format(options.batchsize))
+    print('| Learning rate  : {}'.format(options.lr))
+    print('| Training size   : {}'.format(N_train))
+    print('| Validation size : {}'.format(N_val))
+    print('| Cuda   : {}'.format(options.gpu))
+    print('+------------------------------+')
